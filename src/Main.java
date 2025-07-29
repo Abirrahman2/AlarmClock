723418e5ac4d96aafc86b1858d25cbb4373c07c9
MD MAHMUDUR RAHMAN <105537287+Abirrahman2@users.noreply.github.com>
2025-07-29 11:21:40 +0600

    successfully format the time

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name=scanner.nextLine();
-        System.out.println(name);
 
+        //first i need to format the input, so i format it hour minute and second.
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println("Enter your time for alarm (HH:MM:SS): ");
+        String myTime=scanner.nextLine();
+        LocalTime time=LocalTime.parse(myTime,formatter);
+        System.out.println(time);
+        scanner.close();
     }
 }
